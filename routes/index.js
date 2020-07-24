dc1d4e55976d92387b9739753dbf3f5e432ec0aa
Michael Cook <mcook0775@gmail.com>
2020-07-24 17:45:53 -0400

    Pretty up

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const { validationResult } = require("express-validator")
 
-const { User } = require('../models/index')
+const { User } = require("../models/index")
 
 // validators
 const newUserValidator = require("../validation/newUserValidator")
