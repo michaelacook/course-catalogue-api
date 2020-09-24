8caeed778504228a8caafb49ce2014efab02183b
Michael A. Cook <mcook0775@gmail.com>
2020-09-23 21:55:23 -0400

    Fix typo

--- migrations/20200924002616-create-course.js
+++ migrations/20200924002616-create-course.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references: {
           model: {
-            tableName: "users"
+            tableName: "user"
           },
           key: "id"
         },
