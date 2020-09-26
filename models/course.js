f37ed56276ca9f856bd5de98fe284f4118e152a0
Michael A. Cook <mcook0775@gmail.com>
2020-09-26 15:36:00 -0400

    Manually specify foreign key in association

--- models/course.js
+++ models/course.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate({ User }) {
-      Course.belongsTo(User)
+      Course.belongsTo(User, { foreignKey: 'userId' })
     }
   };
   Course.init({
--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate({ Course }) {
-      User.hasMany(Course)
+      User.hasMany(Course, { foreignKey: 'userId' })
     }
   };
   User.init({
